10083a7167e9c945042eec26a6dc1032cc6d2470
Kedric Webster <kedricwebster@gmail.com>
2017-07-18 20:35:13 -0400

    Greeting JS Style

--- greeting.js
+++ greeting.js
@@ -1,3 +1,3 @@
 var name = prompt("Hi there! What's your name?");
 var output = document.querySelector("#greeting");
-output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+output.innerHTML = "Thanks for visiting, " + name + "!";
