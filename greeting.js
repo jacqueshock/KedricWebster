089fbe6cc6d6ccfd73d7bbf03bb0b758cf6cb252
Kedric Webster <kedricwebster@gmail.com>
2017-07-24 10:32:47 -0400

    Greeter 3.0

--- greeting.js
+++
@@ -1,3 +0,0 @@
-var name = prompt("Hi there! What's your name?");
-var output = document.querySelector("#greeting");
-output.innerHTML = "Thanks for visiting, " + name + "!";
---
+++ media/greeting.js
@@ -0,0 +1,9 @@
+var firstName = prompt("Hi there! What's your first name?");
+var lastName = prompt("What's your last name?");
+var output = document.querySelector('#greeting');
+
+if(firstName && lastName){
+  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
+} else {
+  output.innerHTML = "<p>Please tell us your first and last names!</p>";
+}
