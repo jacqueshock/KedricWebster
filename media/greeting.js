1cac20f4359692ae86e250ea2d63aa6a4abb4fbb
Kedric Webster <kedricwebster@gmail.com>
2017-07-24 11:19:33 -0400

    Using Or ||

--- media/greeting.js
+++ media/greeting.js
@@ -1,9 +1,5 @@
-var firstName = prompt("Hi there! What's your first name?");
-var lastName = prompt("What's your last name?");
+var firstName = prompt("Hi there! What's your first name?") || "Visitor";
+var lastName = prompt("What's your last name?"); || "McDefaultson";
 var output = document.querySelector('#greeting');
 
-if(firstName && lastName){
-  output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
-} else {
-  output.innerHTML = "<p>Please tell us your first and last names!</p>";
-}
+output.innerHTML = "<p>Thanks for visiting, " + firstName + " " + lastName + ".</p>";
