f618d5f211123207be813fbb3a16e29fd812b13f
Kedric Webster <kedricwebster@gmail.com>
2017-07-25 20:55:10 -0400

    jquery

---
+++ projects/jquery/script.js
@@ -0,0 +1,8 @@
+$("h1").css("color", "blue");
+$("h3").css("color", "blue");
+$("p").css("color", "#08088A")
+$("body").css({"background-color": "#EFFBF5", 'padding': '20px'});
+$(".holder1").css({'border': '1px solid red', 'margin':'0 60% 0 10%'});
+$(".holder2").css({'border': '1px solid blue', 'margin-right':'50%'});
+$("#secret").hide();
+$("#change-me").css("color", "green");
