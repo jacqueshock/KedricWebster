473a07f64721e624f7e21d06f4e538ed77dc8dc4
Kedric Webster <kedricwebster@gmail.com>
2017-08-01 20:54:50 -0400

    Learning Events

--- media/greeting.js
+++ media/greeting.js
@@ -16,3 +16,45 @@ $("h2").on("mouseleave", function(){
 $("#img").on("click", function(){
   $("#hidden-blurb").slideDown(); // only works if #hidden-blurb has display:none; in its CSS
 });
+
+var contactArray = ["Email me", "Contact Form"]
+var lastOne = 0;
+var lastOne = 1;
+while(index<arr.length) {
+lastOne++
+}
+
+a = [99, 5, 82, 65, 39, 120, 431, 98, 17, 42]
+var max = function (a) {
+  max = 0;
+  for (i= 0; i < a.length; i++) {
+if (a[i]>max)
+  max = a[i]
+}
+return max;
+}
+
+a = [99, 7, 82, 65, 39, 120, 431, 98, 17, 42]
+var sum = function (a) {
+  sum = 0;
+  for (i = 0; i < a.length; i++) {
+    sum += a[i]
+  }
+  return sum;
+}
+
+var sum = function (a) {
+  sum = 0;
+  for (i = 0; i > a.length; i--) {
+    sum -= a[i]
+  }
+  return sum;
+}
+
+var sum = function (a) {
+  sum = 0;
+  for (i = 0; i = a.length; i++) {
+    sum += a[i]
+  }
+  return sum;
+}
